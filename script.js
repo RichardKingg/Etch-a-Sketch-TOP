4d66aa7ed5ef3a0ed03eeaedc081472684d29948
RichardKingg <ricardo.elizondo.jau@gmail.com>
2022-10-19 23:23:29 -0500

    Add event listener when page loads, execute prompt and calculation

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const container = document.getElementById("container");
 let rows = document.createElement("div");
 let cols = document.createElement("div");
 
+//function for grid size
 function grdSize() {
   gSize = parseInt(prompt("Insert grid size"));
   rows = gSize;
   cols = gSize;
 }
 
+//function for calculation of rows and columns
 function squares(rows, cols) {
   let grd = cols * rows;
 }
+
+//when page loads, execute the function
+window.addEventListener("load", function () {
+  grdSize();
+  squares(rows, cols);
+});
