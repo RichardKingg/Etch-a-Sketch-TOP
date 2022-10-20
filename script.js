445891e41ce4b0f73188f1fd8daec26b98d5dbb3
RichardKingg <ricardo.elizondo.jau@gmail.com>
2022-10-19 22:54:01 -0500

    Add 2 functions, one for user prompt, the other for grid size

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const container = document.getElementById("container");
-const rows = document.createElement("div");
-const cols = document.createElement("div");
+let rows = document.createElement("div");
+let cols = document.createElement("div");
+
+function grdSize() {
+  gSize = parseInt(prompt("Insert grid size"));
+  rows = gSize;
+  cols = gSize;
+}
+
+function squares(rows, cols) {
+  let grd = cols * rows;
+}
