de9587a41b049165e0875e4b2d8968bb899b953d
RichardKingg <ricardo.elizondo.jau@gmail.com>
2022-10-20 19:40:46 -0500

    Rework function for rows and columns

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const container = document.getElementById("container");
-let rows = document.createElement("div");
-let cols = document.createElement("div");
+let rows = document.getElementsByClassName("gRow");
+let cols = document.getElementsByClassName("gCol");
 
-//function for grid size
+//function for grid size, input from user
 function grdSize() {
   gSize = parseInt(prompt("Insert grid size"));
-  rows = gSize;
-  cols = gSize;
+  if (gSize >= 100) {
+    alert("please select a number below 100");
+    location.reload();
+  }
 }
 
-//function for calculation of rows and columns
-function squares(rows, cols) {
-  let grd = cols * rows;
+function mkRows(gSize) {
+  //this loop is for rows
+  for (let r = 0; r < gSize; r++) {
+    let row = document.createElement("div");
+    container.appendChild(row).className = "gRow";
+  }
+}
+
+function mkCols(gSize) {
+  //this loop is for columns
+  for (let c = 0; c < rows.length; c++) {
+    for (let j = 0; j < gSize; j++) {
+      let cols = document.createElement("div");
+      rows[j].appendChild(cols).className = "gCol";
+    }
+  }
 }
 
 //when page loads, execute the function
 window.addEventListener("load", function () {
   grdSize();
-  squares(rows, cols);
+  mkRows(gSize);
+  mkCols(gSize);
 });
